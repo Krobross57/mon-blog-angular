a7c7f259297b6e6a631d1f4b6fc408a51c932a79
antho <krobross_57@hotmail.fr>
2018-12-30 15:42:48 +0100

    Modification du template tet du TypeScript post-list-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mon-blog-angular';
+  isAuth = false;
+
+  onPostLoveItUp() {
+
+  }
 }
--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -10,6 +10,7 @@ export class PostListItemComponent implements OnInit {
   postTitle = 'Ta mère en string de guerre !!!';
   postContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   postLoveIts = 56;
+  postDontLoveIts = 76;
   postDate = '26/12/2018 à 20:32';
 
   constructor() { }
@@ -19,6 +20,32 @@ export class PostListItemComponent implements OnInit {
 
   getContent() {
     return this.postContent;
+  }
+
+  getPostLoveIts() {
+    return this.postLoveIts;
+  }
+
+  getPostDontLoveIts() {
+    return this.postDontLoveIts;
+  }
+
+  getPostDate() {
+    return this.postDate
+  }
+
+  increasePostLoveIts() {
+    this.postLoveIts ++;
+    if (this.postDontLoveIts > 0) {
+      this.postDontLoveIts --;
+    }
+  }
+
+  increasePostDontLoveIts() {
+    this.postDontLoveIts ++;
+    if (this.postLoveIts > 0) {
+      this.postLoveIts --;
+    }
   }
 
 }
