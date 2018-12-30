507e2a93153369129c1454357dbbc71e9a9a055c
antho <krobross_57@hotmail.fr>
2018-12-30 16:03:52 +0100

    Modification du template et dy TypeScript de post-list-item

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -9,8 +9,8 @@ export class PostListItemComponent implements OnInit {
 
   postTitle = 'Ta mère en string de guerre !!!';
   postContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  postLoveIts = 56;
-  postDontLoveIts = 76;
+  postLoveIts = 0;
+  postDontLoveIts = 0;
   postDate = '26/12/2018 à 20:32';
 
   constructor() { }
